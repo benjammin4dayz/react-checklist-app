6e0e8c0e094beebacfeedc8436a328ad2b917844
Jam <42326027+benjammin4dayz@users.noreply.github.com>
2024-07-12 01:05:32 -0400

    Refactor and style task status icon * Add a check to prevent abrupt and potentially misleading color transitions * Add 'touch' handlers in attempts to improve mobile experience * Use effect and state to manage icon color * Use Mui Avatar component in lieu of div container

--- src/components/TaskCard/TaskStatusIcon.tsx
+++ src/components/TaskCard/TaskStatusIcon.tsx
@@ -1,42 +1,71 @@
-import { useState, type FC } from 'react';
+import {
+  useEffect,
+  useState,
+  type ComponentPropsWithoutRef,
+  type FC,
+} from 'react';
+import { Avatar } from '@mui/material';
 import { CheckCircle } from '@mui/icons-material';
 
 type TaskStatusIconProps = {
   checked: boolean;
-} & JSX.IntrinsicElements['div'];
+  colorChangeCooldown?: number;
+} & ComponentPropsWithoutRef<typeof Avatar>;
 
 export const TaskStatusIcon: FC<TaskStatusIconProps> = ({
   checked,
+  colorChangeCooldown = 1000,
   ...restProps
 }) => {
-  const [isHovered, setIsHovered] = useState(false);
+  const [iconColor, setIconColor] = useState<
+    'gray' | 'warning.main' | 'primary.main' | 'success.main'
+  >('gray');
+  const [isHot, setIsHot] = useState<boolean>(false);
+  const [isHovered, setIsHovered] = useState<boolean>(false);
 
   const handleMouseEnter = () => setIsHovered(true);
   const handleMouseOut = () => setIsHovered(false);
+  const handleTouchStart = () => setIsHovered(true);
+  const handleTouchEnd = () => setIsHovered(false);
 
-  let iconColor = 'gray';
+  useEffect(() => {
+    setIsHot(true);
 
-  if (isHovered && checked) {
-    iconColor = 'warning.main';
-  } else if (isHovered) {
-    iconColor = 'primary.main';
-  } else if (checked) {
-    iconColor = 'success.main';
-  }
+    const timeout = setTimeout(() => {
+      setIsHot(false);
+    }, colorChangeCooldown);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [checked, colorChangeCooldown]);
+
+  useEffect(() => {
+    if (checked && isHovered && !isHot) {
+      setIconColor('warning.main');
+    } else if (isHovered && !isHot) {
+      setIconColor('primary.main');
+    } else if (checked) {
+      setIconColor('success.main');
+    } else {
+      setIconColor('gray');
+    }
+  }, [checked, isHovered, isHot]);
 
   return (
-    <div
+    <Avatar
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseOut}
+      onTouchStart={handleTouchStart}
+      onTouchEnd={handleTouchEnd}
+      sx={{
+        backgroundColor: iconColor,
+        opacity: checked ? 1 : 0.7,
+        transition: `background-color ${colorChangeCooldown / 2}ms ease`,
+      }}
       {...restProps}
     >
-      <CheckCircle
-        sx={{
-          color: iconColor,
-          opacity: checked ? 1 : 0.7,
-          transition: 'color 0.3s ease',
-        }}
-      />
-    </div>
+      <CheckCircle sx={{ color: 'text.primary' }} />
+    </Avatar>
   );
 };
